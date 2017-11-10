f853d01bc381c621f7a3e1e40c782969ad6bcbc3
andrel6392 <andrel6392@hstat.org>
2017-11-10 14:39:06 +0000

    pass last two rspec with same code

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(arr)
-    
+    if arr.length == 1
+        arr.join
+    elsif arr.length == 2
+        arr.join(" and ")
+    elsif arr.length >= 3
+        add_and = "and " + arr.pop
+        arr << add_and
+        arr.join(", ")
+    end
 end
