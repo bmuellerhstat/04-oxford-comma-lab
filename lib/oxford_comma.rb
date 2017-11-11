b81d70505eb5bc7fbc538d7d77c50451c4b3c5e2
ricardop2476 <ricardop2476@hstat.org>
2017-11-11 18:54:34 +0000

    complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(arr)
-    
+    if arr.length == 2
+        arr.join(" and ")
+    elsif arr.length == 1
+        arr * ", "
+    else
+        all_but_last = arr.take(arr.length-1)
+        comma_join = all_but_last.join(", ")
+        return "#{comma_join}, and #{arr.last}"
+    end
 end
