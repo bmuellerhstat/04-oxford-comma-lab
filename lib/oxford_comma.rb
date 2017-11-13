df04023202d8ea313f6d4d9c2584a9cca0ca8b64
abdelk7344 <abdelmonemmoamk7344@hstat.org>
2017-11-13 01:38:06 +0000

    use return value for final word

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(arr)
-    
+    if arr.length > 2
+        arr.insert(arr.length-1,"and "+ arr.pop)
+        arr.join(", ")
+    elsif arr.length==2 
+        arr.insert(1,"and")
+        arr.join(" ")
+    else
+        arr.join("")
+    end 
 end
