be200e5be0647bf67045a4f4d8cc527e831e2579
Amr Almaz <amra0760@hstat.org>
2017-11-26 05:04:53 +0000

    Cleaned it up

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,13 @@
 def oxford_comma(arr)
-    if arr.length < 2
-        arr.join()
-    elsif arr.length == 2
+    if arr.length == 2
         arr.join(" and ")
     elsif arr.length > 2
-        
         last_element = arr[-1]
         arr.pop
         arr.insert(-1,"and " + last_element)
-        # arr.insert(-2, 'and')
         arr.join(", ")
-        # i = arr.index {|el| el =~ /\d{3}/ }
-        # i= arr.length
-        
-        # arr.delete_at(-2)
-        # arr.join(", ") + arr[i..i-1].join(" ") 
-        # arr[arr.length-3]
-        
     else
-        arr.join(", ")
+        arr.join()
     end
     
 end
