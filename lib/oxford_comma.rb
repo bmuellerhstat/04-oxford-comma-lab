f3826639f9d327e6948377a093c5d6b613d4c629
simiek <simeonk3817@hstat.org>
2017-12-07 02:14:26 +0000

    Extra credit done!

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(arr)
-    
+    if arr.length == 2
+     arr.join(" and ")
+    elsif arr.length >= 3
+    last_element= arr.pop
+    arr << "and " 
+    arr.join(", ") + last_element.to_s
+    else 
+        arr.join(", ")
+    end
 end
