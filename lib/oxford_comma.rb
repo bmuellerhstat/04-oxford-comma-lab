a586dd5e5dd9139c22c3810071410232b8126eed
Yu Hang Chen <yuhangc9321@hstat.org>
2018-11-07 20:48:40 +0000

    define #oxford_comma to pass tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(arr)
-    
+    if arr.length > 2
+        arr[-1] = "and #{arr.last}"
+        return arr[0..-1].join(", ")
+    elsif arr.length > 1
+        return arr.join(" and ")
+    else
+        return arr[0]
+    end
 end
