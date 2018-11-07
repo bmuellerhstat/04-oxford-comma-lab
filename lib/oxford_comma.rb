98db6c6d7440ddeab240545f4526f499f1b185c9
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-11-07 21:10:41 +0000

    finished EC

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(arr)
-    
+rra = []
+    if arr.length==1
+        arr.shift
+    elsif arr.length==2
+        item1 = arr.shift
+        item2 = arr.shift
+        arr = [item1," and ", item2].join
+    else
+        arr.each do |item|
+            if item != arr[arr.length-1]
+             rra << item+", "
+         else
+             rra << "and "+item 
+        end
+    end
+        return rra.join
+    end
 end
