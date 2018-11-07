67f6d80d903e0315a8f05b2d45f3ad1d4c36954f
Ahmad Rabah <ahmadr9279@hstat.org>
2018-11-07 23:13:11 +0000

    Fix all errors in converting array to a string

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(arr)
-    
+def oxford_comma(arr=[])
+first_part = arr[0..-2].join(", ")
+last_part = arr[-1]
+if arr.length > 2 
+first_part+", and "+last_part
+elsif arr.length > 1
+arr.join(" and ")
+elsif arr.length == 1
+arr[1]
 end
+end
+oxford_comma("apples")
