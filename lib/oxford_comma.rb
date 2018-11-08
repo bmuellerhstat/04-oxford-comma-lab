a20db898394738874e9bc6e47db79b6259f57b05
Eva Noertoft <evan2304@hstat.org>
2018-11-08 05:16:22 +0000

    pass all tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+inp = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 def oxford_comma(arr)
-    
+    if arr.empty?
+        return nil 
+    elsif arr.length == 1 
+        return arr.join
+    elsif arr.length == 2
+        return arr.join(" and ")
+    elsif arr.length == 3
+    arr[0..1].join(", ") << ", and " << arr[2].to_s
+    else
+    arr[0...-1].join(", ") << ", and " << arr.last.to_s
+    end 
 end
+
+oxford_comma(inp)
