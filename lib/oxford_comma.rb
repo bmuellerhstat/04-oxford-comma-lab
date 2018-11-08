fff41fee7c0fe3bef1a8dfac19ffc46a6af18f86
Raquel Joseph <raquelj6437@hstat.org>
2018-11-08 18:34:52 +0000

    pass rspec

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(arr)
-    
+    if arr.count == 1
+        arr[0]
+    elsif arr.count == 2
+        arr.join(' and ')
+    else
+        last = ", and " + arr.last
+        arr[0...arr.count-1].join(', ') + last
+    end
 end
