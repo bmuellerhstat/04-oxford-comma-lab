de76e7041373b1ff7d4d0a520852c08815034638
Johnson Wu <johnsonw6558@hstat.org>
2018-11-08 04:14:49 +0000

    complete EC

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(arr)
-    
+    if arr.length == 1
+        arr[0]
+    elsif arr.length == 2
+        arr.join(" and ")
+    elsif arr.length >= 3
+        and_array = ["and"]
+        and_array << arr.pop() 
+        arr << and_array.join(" ")
+        arr.join(", ")
+    end
 end
+
+array = ["ice", "bread", "egg"]
+oxford_comma(array)
