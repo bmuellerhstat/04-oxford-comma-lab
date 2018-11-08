a45e27027403f304b079cae5a312881e0eaef89e
Xiurong Yu <xiurongy3506@hstat.org>
2018-11-08 03:09:21 +0000

    split elements in an array

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(arr)
-    
+    if arr.count == 1
+        arr.join()
+        
+    elsif arr.count ==2
+        arr.join(" and ")
+        
+    else 
+        last_fruit = arr.pop
+        arr.join(", ") + ", and " + last_fruit
+    end
 end
