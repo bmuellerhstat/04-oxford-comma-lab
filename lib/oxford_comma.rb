c2c8572606fef4b4af8a3165d41a9883751b27a3
Kasper Gacek <kasperg3971@hstat.org>
2018-11-08 03:26:35 +0000

    EC done passed tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(arr)
-    
+     if arr.length == 1
+         arr.join
+     elsif arr.length == 2
+         arr.join(" and ")
+     else
+         last = arr.slice!(-1)
+         comma = arr.join(", ")
+         comma << ", and " + last
+     end
 end
+
+
+# def oxford_comma(arr)
+#      if arr.length == 1
+#          arr.join
+#      elsif arr.length == 2
+#          arr.join(" and ")
+#      else
+#          arr.slice!(-1)
+#          comma = arr[0..2].join(", ")
+#          comma << ", and  #{arr.last}"
+#      end
+# end
