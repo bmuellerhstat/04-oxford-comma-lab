18ebdf5285a79e2e64106d2b23d620808b91eb50
Wei Zheng <weiz9762@hstat.org>
2018-11-08 02:15:48 +0000

    format serial comma with conditional

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(arr)
-    
+    if arr.length == 1
+        arr.join
+    elsif arr.length == 2 
+        arr.join(" and ")
+    else
+        # select all but last item
+        arr[0...-1].each do |item| # add comma after it
+            item << ","
+        end
+        arr.insert(-2, "and") # add "and" before last item
+        arr.join(" ")
+    end
 end
