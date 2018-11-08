0ce355a52f3f72bf08940d5a4e53a79a0335af2f
JENNIFER LIU <jenniferl4209@hstat.org>
2018-11-08 03:20:04 +0000

    separate array items with commas

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(arr)
-    
+    if arr.length == 1
+        arr.join #array convert to string
+    elsif arr.length == 2
+        arr.join (" and ") #will place an occurrence of that  (and between each element)
+    else
+        last_element = arr.pop #remove the last element in an array - need to store in a variable in order to use the last element
+        arr.join(", ") + ", and " + last_element
+    end
 end
