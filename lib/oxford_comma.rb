7136dcb0f4b18fb09667253a22fafcac2042c301
Xavier Jiang <xavierj2216@hstat.org>
2018-11-08 01:26:24 +0000

    finish extra credit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(arr)
-    
+    if arr.length == 2
+        arr.join(" and ")
+    elsif arr.length == 1
+        arr.flatten.join
+    else
+        last = arr.pop
+        arr.join(", ") + ", and " + last.to_s    
+end
 end
