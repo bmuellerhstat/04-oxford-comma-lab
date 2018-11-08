3c3f5f859a4db582e6ee8870c36287d895325301
Kasper Gacek <kasperg3971@hstat.org>
2018-11-08 03:28:50 +0000

    EC RSPEC done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,16 +9,3 @@ def oxford_comma(arr)
          comma << ", and " + last
      end
 end
-
-
-# def oxford_comma(arr)
-#      if arr.length == 1
-#          arr.join
-#      elsif arr.length == 2
-#          arr.join(" and ")
-#      else
-#          arr.slice!(-1)
-#          comma = arr[0..2].join(", ")
-#          comma << ", and  #{arr.last}"
-#      end
-# end
