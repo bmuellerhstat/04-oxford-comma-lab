a6a712d5e8e1771c043ec5682f1be22c0c17d8bb
Shi Wei Zheng <shiz3150@hstat.org>
2018-11-08 00:10:03 +0000

    pass all tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(arr)
-    
+    if arr.length == 1
+        arr.join
+    elsif arr.length == 2
+        arr.join(" and ")
+    else
+        last_value = arr.pop
+        add_comma = arr.join(", ")
+        add_comma << ", and " + last_value
+    end
 end
